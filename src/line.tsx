4d96c36c79aa6502b345f54ae4b22d17325710ae
Leanne Werner <leanne.werner.lw@gmail.com>
2022-02-01 15:34:10 -0800

    adding measure test

--- src/line.tsx
+++ src/line.tsx
@@ -1,4 +1,4 @@
-import { Line } from "@react-three/drei";
+import { Html, Line } from "@react-three/drei";
 import * as React from "react";
 import * as THREE from "three";
 import { Measure } from "./Effects";
@@ -153,6 +153,7 @@ export function MeasurementsFromBoundingBox(
       multiSelectBoundingBox.max.z
     ),
   ];
+
   return (
     <>
       <Line
@@ -176,83 +177,34 @@ export function MeasurementsFromBoundingBox(
         dashed={true}
         dashScale={5.0}
       />
-      <Line points={crossX} color={"#932191"} lineWidth={1.0} />
-      <Line points={crossY} color={"#932191"} lineWidth={1.0} />
+      <Line points={crossX} color={"#932191"} lineWidth={1.0}>
+        <group position={getMidPoint(crossX)}>
+          <Html
+            as="div"
+            wrapperClass="measure-text"
+            zIndexRange={[100, 0]}
+            center
+            transform={false}
+            sprite={true}
+          >
+            <p>{Math.round(Math.abs(crossX[0].x - crossX[1].x))}</p>
+          </Html>
+        </group>
+      </Line>
+      <Line points={crossY} color={"#932191"} lineWidth={1.0}>
+        <group position={getMidPoint(crossY)}>
+          <Html
+            as="div"
+            wrapperClass="measure-text"
+            zIndexRange={[100, 0]}
+            center
+            transform={false}
+            sprite={true}
+          >
+            <p>{Math.round(Math.abs(crossY[0].y - crossY[1].y))}</p>
+          </Html>
+        </group>
+      </Line>
     </>
   );
 }
-
-// export function Measurements(props: Measure) {
-// if (props.hovered.length === 0 || props.selected.length === 0) {
-//   return null;
-// }
-
-// const implicitAxisStartPoint = getFaceVertex(props.hovered[0]);
-// const explicitAxisEndPoint = getFaceVertex(props.selected[0]);
-// const xIntersectPoint = getXIntersect(props.selected[0], props.hovered[0]);
-// const yIntersectPoint = getYIntersect(props.selected[0], props.hovered[0]);
-
-// const pointsIX = useMemo(
-//   () => [implicitAxisStartPoint, xIntersectPoint],
-//   [implicitAxisStartPoint, xIntersectPoint]
-// );
-// const pointsIY = useMemo(
-//   () => [implicitAxisStartPoint, yIntersectPoint],
-//   [implicitAxisStartPoint, yIntersectPoint]
-// );
-// const pointsEX = useMemo(
-//   () => [yIntersectPoint, explicitAxisEndPoint],
-//   [yIntersectPoint, explicitAxisEndPoint]
-// );
-// const pointsEY = useMemo(
-//   () => [xIntersectPoint, explicitAxisEndPoint],
-//   [xIntersectPoint, explicitAxisEndPoint]
-// );
-
-//   return (
-//     <>
-//       <Line
-//         points={pointsIX}
-//         color={"#e17327"}
-//         lineWidth={1}
-//         dashed={true}
-//         dashScale={2}
-//       />
-//       <Line
-//         points={pointsIY}
-//         color={"#e17327"}
-//         lineWidth={1}
-//         dashed={true}
-//         dashScale={2}
-//       />
-//       <Line points={pointsEY} color={"#932191"} lineWidth={2} dashed={false}>
-//         <group position={getMidPoint(pointsEY)}>
-//           <Html
-//             as="div"
-//             wrapperClass="measure-text"
-//             zIndexRange={[100, 0]}
-//             center
-//             transform={false}
-//             sprite={true}
-//           >
-//             <p>{Math.round(Math.abs(pointsEY[0].y - pointsEY[1].y))}</p>
-//           </Html>
-//         </group>
-//       </Line>
-//       <Line points={pointsEX} color={"#932191"} lineWidth={2} dashed={false}>
-//         <group position={getMidPoint(pointsEX)}>
-//           <Html
-//             as="div"
-//             wrapperClass="measure-text"
-//             zIndexRange={[100, 0]}
-//             center
-//             transform={false}
-//             sprite={true}
-//           >
-//             <p>{Math.round(Math.abs(pointsEX[0].x - pointsEX[1].x))}</p>
-//           </Html>
-//         </group>
-//       </Line>
-//     </>
-//   );
-// }

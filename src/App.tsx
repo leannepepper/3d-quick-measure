539bdeba98946cd76c7702280ff48cd3d7912d25
Leanne Werner <leanne.werner.lw@gmail.com>
2022-02-10 13:07:44 -0800

    site wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,62 @@
-import { MeshReflectorMaterial, OrbitControls, Plane } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import "./style.css";
+import { OrbitControls } from "@react-three/drei";
+import {
+  Canvas,
+  extend,
+  useThree,
+  useFrame,
+  ReactThreeFiber,
+} from "@react-three/fiber";
 import * as React from "react";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import { ExampleShapes } from "./Shapes";
 import { MeasureText } from "./MeasureText";
 import { QuickMeasureEffect } from "./QuickMeasureEffects";
 import { GridLines } from "./GridLines";
-import "./style.css";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// extend({ OrbitControls });
+
+// declare global {
+//   namespace JSX {
+//     interface IntrinsicElements {
+//       orbitControls: ReactThreeFiber.Object3DNode<
+//         OrbitControls,
+//         typeof OrbitControls
+//       >;
+//     }
+//   }
+// }
+
+// function Controls() {
+//   const controls = useRef(null);
+//   const { camera, gl } = useThree();
+//   useFrame(() => controls.current.update());
+
+//   return (
+//     <orbitControls
+//       ref={controls}
+//       args={[camera, gl.domElement]}
+//       enableDamping
+//       dampingFactor={0.1}
+//       rotateSpeed={0.5}
+//     />
+//   );
+// }
 
 export default function App() {
+  const controlsRef = useRef();
   return (
     <Canvas camera={{ position: [0, 0, 20], near: 0.1, far: 100, fov: 50 }}>
       <pointLight position={[5, 15, 10]} intensity={1.0} />
       <ambientLight intensity={0.7} />
-      <OrbitControls autoRotate={false} enableZoom={true} enablePan={false} />
+      <OrbitControls
+        ref={controlsRef}
+        autoRotate={false}
+        enableZoom={false}
+        enablePan={false}
+      />
+      {/* <Controls /> */}
       <QuickMeasureEffect>
         <GridLines />
         <Suspense fallback={null}>
--- src/Shapes.tsx
+++ src/Shapes.tsx
@@ -1,18 +1,77 @@
-import { Sphere } from "@react-three/drei";
+import { Plane, Sphere, Cone } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 import * as React from "react";
 
 export const ExampleShapes = ({ ...props }) => {
-  const { camera, gl } = useThree();
+  const { viewport } = useThree();
+  const gridWidth = viewport.width;
+  const gridHeight = viewport.height;
 
   return (
-    <Sphere
-      position={[0, 2, -10]}
-      rotation={[-2.0, -1.0, 0.0]}
-      args={[5, 30, 30, 0]}
-      castShadow
-    >
-      <meshStandardMaterial attach="material" color="#fac407" roughness={0} />
-    </Sphere>
+    <>
+      <Plane
+        position={[-gridWidth / 3, -gridHeight / 3, -0.01]}
+        args={[gridWidth / 3, gridHeight / 3]}
+      >
+        <meshStandardMaterial
+          attach="material"
+          color="#000000"
+          roughness={0.9}
+        />
+      </Plane>
+      <Plane
+        position={[-gridWidth / 3, gridHeight / 12, -0.01]}
+        args={[gridWidth / 3, gridHeight / 6]}
+      >
+        <meshStandardMaterial
+          attach="material"
+          color="#000000"
+          roughness={0.9}
+        />
+      </Plane>
+      <Plane
+        position={[-gridWidth / 3, -gridHeight / 12, -0.01]}
+        args={[gridWidth / 3, gridHeight / 6]}
+      >
+        <meshStandardMaterial
+          attach="material"
+          color="#000000"
+          roughness={0.9}
+        />
+      </Plane>
+      <Plane
+        position={[gridWidth / 3, gridHeight / 3, -0.01]}
+        args={[gridWidth / 3, gridHeight / 3]}
+      >
+        <meshStandardMaterial
+          attach="material"
+          color="#000000"
+          roughness={0.9}
+        />
+      </Plane>
+      <Cone
+        position={[gridWidth / 3, gridHeight / 3, 0]}
+        rotation={[0.22, 1.0, 0]}
+        args={[2, 3, 3, 1]}
+      >
+        <meshStandardMaterial
+          attach="material"
+          color="#fac407"
+          roughness={0.9}
+        />
+      </Cone>
+      <Sphere
+        position={[-gridWidth / 3, -gridHeight / 2.1, 0]}
+        rotation={[-2.0, -1.0, 0.0]}
+        args={[3.5, 60, 30, 0]}
+        castShadow
+      >
+        <meshStandardMaterial
+          attach="material"
+          color="#fac407"
+          roughness={0.9}
+        />
+      </Sphere>
+    </>
   );
 };
